06b58b57d98411004fac9683a4ce081127cea6ee
Vinicius <viniciushpx157@hotmail.com>
2022-12-22 22:57:18 -0300

    first commit

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,33 @@
+exports.meuMiddleware = (req , res , next) => {
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
+    next();
+
+}
+exports.outroMiddleware = (req , res , next) => {
+    next();
+}
+
+exports.checkCsrferror = (err , req , res , next) => {
+    if(err) {
+        return res.render('404');
+    }
+    next();
+}
+
+exports.csrfMiddleware = (req , res , next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+}
+
+exports.loginRequired = (req , res , next) => {
+    if(!req.session.user){
+        req.flash('errors','Você precisa fazer o login.');
+        req.session.save(() => {
+            res.redirect('/');
+        });
+        return;
+    }
+    next();
+}
