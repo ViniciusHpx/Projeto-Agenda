593f52c62f90f82b279daf1298eeed4bc51227a5
Vinicius <viniciushpx157@hotmail.com>
2022-12-27 16:34:36 -0300

    Exibindo e deletando contatos

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -63,4 +63,20 @@ Contato.prototype.edit = async function (id){
     if(this.errors.length > 0) return;
     this.contato = await ContatoModel.findByIdAndUpdate(id , this.body, {new: true});
 }
+
+Contato.buscaPorId = async function(id) {
+    if(typeof id !== 'string') return;
+    const contato = await ContatoModel.findById(id);
+    return contato;
+}
+Contato.buscaContatos = async function() {
+    const contatos = await ContatoModel.find().sort({criadoEm:-1});
+    return contatos;
+}
+Contato.delete = async function(id) {
+    if(typeof id !== 'string') return;
+    const contato = await ContatoModel.findOneAndDelete({_id: id});
+    return contato;
+}
+
 module.exports = Contato;
